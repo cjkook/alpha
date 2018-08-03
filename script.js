20574422264815f7b0e6cdad1e2bf8991c48276e
cjkook <cjko.audio@gmail.com>
2018-08-03 17:49:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+Student Goals
+- Understand that JavaScript has different versions
+  - Buzzword: Javascript, you've heard it before.
+  - Scripting language to make web applications interactive
+    - You've visited simple websites, but you've also visited interactive websites (FB,AirBnB,)
+  - ECMAScript 6 = specification, standardizes JS. Intro'ed 	June 2015
+  -
+- Use core ES6 conventions & standard formatting
+- Understand and use ‘strict’ mode
+*/
